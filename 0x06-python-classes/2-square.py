3d781762682d1f920878e0a41b55514c219f37d2
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2023-06-28 04:57:00 +0300

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,7 +4,7 @@
 class Square:
     """Represent a square."""
 
-def __init__(self, size=0):
+    def __init__(self, size=0):
     """Initialize a new Square.
         Args:
             size (int): The size of the new square.

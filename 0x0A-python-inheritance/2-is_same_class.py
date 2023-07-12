e88fe2635b6e5035dbc6c8d6f67ba4cb46d124b0
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2023-07-12 18:59:44 +0300

    Update 2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -7,4 +7,4 @@ Contains the is same class function
 def is_same_class(obj, a_class):
     """ returns a list of available and methodes of an objet"""
 
-    return (type(obj) == a_class)
+    return (isinstance(obj, a_class))

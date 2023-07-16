184c224c7d486990867c4f9331e6e362d4ed4a73
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2023-07-16 23:05:15 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,14 +6,13 @@ and then save them to a file.
 
 import sys
 import json
-import os.path
 
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 my_file = 'add_item.json'
 my_list = []
-if (load_from_json_file(my_file)):
+if load_from_json_file(my_file):
     my_list = load_from_json_file(my_file)
 for index in range(1, len(sys.argv)):
     my_list.append(sys.argv[index])

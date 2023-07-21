5b3724696cfd1886dad3ba1d00291af4599ca29a
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2023-07-21 23:28:37 +0300

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,11 +7,11 @@ class Rectangle(Base):
     """I am class Rectangle."""
     def __init__(self, width, height, x=0, y=0, id=None):
         """I am init method."""
-        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        super().__init__(id)
 
     @property
     def width(self):

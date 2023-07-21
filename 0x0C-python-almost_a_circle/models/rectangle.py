f463532d4a0d32c3a6fc4fe537669602db11b19a
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2023-07-21 23:56:46 +0300

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,9 +4,23 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    """I am class Rectangle."""
+    """Represent a rectangle."""
+
     def __init__(self, width, height, x=0, y=0, id=None):
-        """I am init method."""
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+            x (int): The x coordinate of the new Rectangle.
+            y (int): The y coordinate of the new Rectangle.
+            id (int): The identity of the new Rectangle.
+        Raises:
+            TypeError: If either of width or height is not an int.
+            ValueError: If either of width or height <= 0.
+            TypeError: If either of x or y is not an int.
+            ValueError: If either of x or y < 0.
+        """
         self.width = width
         self.height = height
         self.x = x
@@ -66,14 +80,21 @@ class Rectangle(Base):
         self.__y = value
 
     def area(self):
-        """Returns the area"""
-        return (self.__width * self.__height)
+        """Return the area of the Rectangle."""
+        return self.width * self.height
 
     def display(self):
-        """prints the rectangle"""
-        for x in range(self.__height):
-            [print("#", end='')for y in range(self.__width)]
-            print()
+        """Print the Rectangle using the `#` character."""
+        if self.width == 0 or self.height == 0:
+            print("")
+            return
+
+        [print("") for y in range(self.y)]
+        for h in range(self.height):
+            [print(" ", end="") for x in range(self.x)]
+            [print("#", end="") for w in range(self.width)]
+            print("")
+
     def update(self, *args, **kwargs):
         """Update the Rectangle.
 
@@ -122,16 +143,16 @@ class Rectangle(Base):
 
     def to_dictionary(self):
         """Return the dictionary representation of a Rectangle."""
-        return ({
+        return {
             "id": self.id,
             "width": self.width,
             "height": self.height,
             "x": self.x,
             "y": self.y
-        })
+        }
 
     def __str__(self):
         """Return the print() and str() representation of the Rectangle."""
-        return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
                                                        self.x, self.y,
-                                                       self.width, self.height))
+                                                       self.width, self.height)

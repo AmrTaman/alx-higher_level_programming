0686ecb441ecd0244e4cabcc8e1f0186ef10891f
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2023-07-21 23:23:48 +0300

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,46 +1,49 @@
 #!/usr/bin/python3
-"""Iam the module rectangle."""
-__import__("base.py")
+from base import Base
 
 
-class Rectangle(Base) :
-  """Iam class Rectangle."""
-  def __init__(self, width, height, x=0, y=0, id=None) :
-    """Iam init method."""
-    super().__init__(self, id)
-    self.width = width
-    self.height = height
-    self.x = x
-    sefl.y = y
+class Rectangle(Base):
+    """I am class Rectangle."""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """I am init method."""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
-  @property
-  def width(self) :
-    """Iam the setter and getter."""
-    return (self.__width)
-  @width.setter
-  def width(self, w) :
-    self.__width = w
+    @property
+    def width(self):
+        """I am the setter and getter."""
+        return self.__width
 
-  @property
-  def height(self) :
-    """Iam the setter and getter."""
-    return (self.__height)
-  @height.setter
-  def height(self, h) :
-    self.__height = h
+    @width.setter
+    def width(self, w):
+        self.__width = w
 
-  @property
-  def x(self) :
-    """Iam the setter and getter."""
-    return (self.__x)
-  @x.setter
-  def x(self, x) :
-    self.__x = x
+    @property
+    def height(self):
+        """I am the setter and getter."""
+        return self.__height
 
-  @property
-  def y(self) :
-    """Iam the setter and getter."""
-    return (self.__y)
-  @y.setter
-  def y(self, y) :
-    self.__y = y
+    @height.setter
+    def height(self, h):
+        self.__height = h
+
+    @property
+    def x(self):
+        """I am the setter and getter."""
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        self.__x = x
+
+    @property
+    def y(self):
+        """I am the setter and getter."""
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        self.__y = y

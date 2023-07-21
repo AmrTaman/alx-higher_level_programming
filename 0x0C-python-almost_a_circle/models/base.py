2014c968ff98c9fa37e76260ad8d570a9755fb47
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2023-07-21 22:24:33 +0300

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""Iam the module"""
+
+
+class Base:
+  """This is class base"""
+
+  __nb_objects = 0
+  def __init__(self, id=None):
+    """Iam method init"""
+
+    if id != None :
+      self.id = id
+    else :
+      __nb_objects += 1
+      self.id = __nb_objectss
+      

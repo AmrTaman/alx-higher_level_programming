9693b139519ad2ed3cfd7df9c760023b026aea3a
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2023-10-05 02:34:14 +0300

    Update 6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -10,9 +10,11 @@ class Rectangle:
     class Rectangle for defining properties of a rectangle
     """
 
+    number_of_instances = 0
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+        number_of_instances += 1
 
     @property
     def width(self):
@@ -62,3 +64,7 @@ class Rectangle:
         if self.width == 0 or self.height == 0:
             return ("")
         return "Rectangle({}, {})".format(self.width, self.height)
+
+    def __del__(self):
+        number_of_instances -= 1
+        print("Bye rectangle...")

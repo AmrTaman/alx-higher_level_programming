c103cb82cadbc6ecfb772ffe5cba34379f00e69d
AmrTaman <amrtaman4325@gmail.com>
2024-04-08 21:38:37 +0000

    d

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
-const argLength = process.argv.length;
+const argsCount = process.argv.length - 2;
 
-if (argLength <= 2) {
+if (argsCount === 0) {
   console.log('No argument');
-} else if (argLength > 2) {
+} else if (argsCount === 1) {
   console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }

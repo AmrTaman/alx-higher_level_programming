91bed6e053afb9917ca5c40cd3a0575c640ba15a
AmrTaman <amrtaman4325@gmail.com>
2024-04-10 01:25:47 +0000

    d

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const Sq = require('./5-square');
+
+class Square extends Sq {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+      return;
+    }
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'C';
+      }
+      console.log(row);
+    }
+  }
+}
+
+module.exports = Square;

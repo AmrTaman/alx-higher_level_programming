6d020ce00c7ff6d8f427de1eaf763ee9a61fa2de
AmrTaman <amrtaman4325@gmail.com>
2024-04-13 16:13:55 +0000

    d

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+you turn the lights in then you get them back off
+"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    args = (sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=args[0],
+            passwd=args[1],
+            db=args[2]
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE %s"
+                " ORDER BY id ASC", (args[3],))
+    row = cur.fetchone()
+    print(row)
+    cur.close()
+    db.close()

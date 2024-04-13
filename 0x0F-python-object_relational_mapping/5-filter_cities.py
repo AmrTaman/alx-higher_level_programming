eb39b2ca58fbf91b4ba35fa7a1ee48d9a68e3e09
AmrTaman <amrtaman4325@gmail.com>
2024-04-13 19:26:56 +0000

    d

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -9,18 +9,23 @@ import MySQLdb
 
 
 if __name__ == "__main__":
-  username, password, database, state_name = sys.argv[1:]
-  db = MySQLdb.connect(
-          host="localhost",
-          port=3306,
-          user=username,
-          passwd=password,
-          db=database
-  )
-  cur = db.cursor()
-  cur.execute("SELECT * FROM states WHERE BINARY name LIKE '{}'"
-              " ORDER BY id ASC".format(state_name))
-  rows = cur.fetchall()
-  [print(row) for row in rows]
-  cur.close()
-  db.close()
+    username, password, database, state_name = sys.argv[1:]
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+    )
+    cur = db.cursor()
+    cur.execute("SELECT cities.name FROM states JOIN cities ON"
+                " cities.state_id = states.id WHERE states.name = %s"
+                " ORDER BY cities.id ASC", (state_name,))
+    rows = cur.fetchall()
+    cities = [row[0] for row in rows]
+    if cities:
+        print(', '.join(cities))
+    else:
+        print()
+    cur.close()
+    db.close()

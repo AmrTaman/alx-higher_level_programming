b0e92bc73a09560a2bf7d23602ba1176533ee0ad
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2024-04-13 20:53:35 +0200

    Create 5-filter_cities.py

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+you turn the lights in then you get them back off
+"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+  username, password, database, state_name = sys.argv[1:]
+  db = MySQLdb.connect(
+          host="localhost",
+          port=3306,
+          user=username,
+          passwd=password,
+          db=database
+  )
+  cur = db.cursor()
+  cur.execute("SELECT * FROM states WHERE BINARY name LIKE '{}'"
+              " ORDER BY id ASC".format(state_name))
+  rows = cur.fetchall()
+  [print(row) for row in rows]
+  cur.close()
+  db.close()

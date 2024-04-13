3c84af0b8ebb9c533652bbb77e636e94ff35bdbf
AmrTaman <amrtaman4325@gmail.com>
2024-04-13 23:54:41 +0000

    d

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,7 +10,7 @@ from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 
 
-engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format
+engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
                        (sys.argv[1], sys.argv[2],
                         sys.argv[3]), pool_pre_ping=True)
 Base.metadata.create_all(bind=engine)

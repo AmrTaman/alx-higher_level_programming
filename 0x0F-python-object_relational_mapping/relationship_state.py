4da98497286934f0276ef603f33da8b1534c5fe9
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2024-04-14 14:49:54 +0200

    Create relationship_state.py

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+this module is the mapping declare module
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    define a sqlalchemy for a db class state
+    """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state = relationship("City", cascade="all, delete")

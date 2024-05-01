21e2b091478ac12db35d856d2ab310d084e360e8
AmrTaman <amrtaman4325@gmail.com>
2024-05-01 14:02:01 -0700

    d

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+iam here
+"""
+
+
+if __name__ == '__main__':
+    import sys
+    import requests
+
+    r = requests.get('https://api.github.com/user', auth=(sys.argv[1], sys.argv[2]))
+    print(r.json().get('id'))

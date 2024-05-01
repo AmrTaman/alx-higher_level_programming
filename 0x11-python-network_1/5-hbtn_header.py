4d954abf8b94af8f1276df7890147b2c21f362f2
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2024-05-01 21:03:05 +0300

    Update 5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-
-"""
-module for getting a request
+"""module for getting a request
+dddddd
 """
 
 import requests
 import sys
-
 
 r = requests.get(sys.argv[1])
 print(r.headers['X-Request-Id'])

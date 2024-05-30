96c38671bb3269b8ac198ddd45d74aed3f6de9c5
AmrTaman <amrtaman4325@gmail.com>
2024-05-30 11:01:09 +0000

    d

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,8 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, function (err, response, body) {
+  if (err) {
+    return;
+  }
   console.log('code:', response.statusCode);
 });
